5f855d57870d6b3d918af979f9fdea40cab56cb7
Bruk Teshome <brukteshome21@gmail.com>
2024-07-15 16:57:18 +0300

    Sruct: added for token holder info

--- contracts/LiquidityMarket.sol
+++ contracts/LiquidityMarket.sol
@@ -9,4 +9,15 @@ contract LiquidityMarketPlace {
   address public ownerOfContract;
   uint256 public userId;
   address[] public holderToken;
+
+  event transfer (address indexed _from, address indexed _to, uint256 _amount);
+  event approval (address indexed _owner, address indexed _spender, uint256 _amount);
+
+  struct tokenHolderInfo {
+    uint256 _tokeId;
+    address _from;
+    address _to;
+    uint256 _totalToken;
+    bool _tokenHolder;
+  }
 }
