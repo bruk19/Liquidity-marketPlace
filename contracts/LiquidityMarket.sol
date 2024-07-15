19462058b3169d18b5e230b48613f6623b454ba3
Bruk Teshome <brukteshome21@gmail.com>
2024-07-15 16:11:24 +0300

    State variable: added

--- contracts/LiquidityMarket.sol
+++ contracts/LiquidityMarket.sol
@@ -2,4 +2,11 @@
 pragma solidity ^0.8.24;
 
 contract LiquidityMarketPlace {
+  string public name;
+  string public symbol; 
+  string public standard;
+  uint256 public totalSupply;
+  address public ownerOfContract;
+  uint256 public userId;
+  address[] public holderToken;
 }
