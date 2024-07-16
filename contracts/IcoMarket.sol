4c0369602c4a55c372ff79e0ae7d5f84a75801d8
Bruk Teshome <brukteshome21@gmail.com>
2024-07-16 21:05:24 +0300

    State Variable: added to the contract

--- contracts/IcoMarket.sol
+++ contracts/IcoMarket.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.24;
 
 import "./LiquidityMarket.sol";
 contract IcoMarket {
+  address admin;
+  LiquidityMarket public tokenContract;
+  uint256 public tokenPrice;
+  uint256 public tokenSold;
   }
--- contracts/LiquidityMarket.sol
+++ contracts/LiquidityMarket.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-contract LiquidityMarketPlace {
+contract LiquidityMarket {
   string public name;
   string public symbol; 
   string public standard;
