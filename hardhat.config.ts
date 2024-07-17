ebfd06759b3e048a70c5f708d4c89954b86d0c2c
Bruk Teshome <brukteshome21@gmail.com>
2024-07-17 13:18:07 +0300

    feat: add dotenv config and RPC URL

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const EHTERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
