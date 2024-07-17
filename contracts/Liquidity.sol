2c7e9f1bfa912151795ad8a5cd3daacba50aedd6
Bruk Teshome <brukteshome21@gmail.com>
2024-07-17 10:14:24 +0300

    Struct: added to store liquidity info

--- contracts/Liquidity.sol
+++ contracts/Liquidity.sol
@@ -2,5 +2,20 @@
 pragma solidity ^0.8.24;
 
 contract Liquidity {
-  
+
+  address public admin;
+  uint256 public liquidityId;
+
+  struct liquidityInfo {
+    uint256 id;
+    address owner;
+    string tokenA;
+    string tokenB;
+    string tokenA_Address;
+    string tokenB_Address;
+    string poolAddress;
+    string network;
+    string transactionHash;
+    uint256 timeCreated;
+  }
 }
